605e1537cc7be19bdba8251f034bfb8e839f5ebb
Aura_Mejia <aurasofiamejia2805@gmail.com>
2021-08-04 23:48:10 -0500

    Agregamos el pie de página con estilos y los botones de yes y no

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,37 @@
-
-
+import React from 'react';
 import './footer.css';
+import Banner from '../../Utils/Img/Logos/banner_grande.png'
+import Twitter from '../../Utils/Img/Iconos/twitter.svg'
+import Facebook from '../../Utils/Img/Iconos/facebook.svg'
+import Youtube from '../../Utils/Img/Iconos/youtube.svg'
+import Instagram from '../../Utils/Img/Iconos/logo-instagram.svg'
+import Barras from '../../Utils/Img/Iconos/barras.svg'
 
 
 function Footer() {
   return (
-<footer className="footindex">
- &copy;Sitio desarrollado  Khadija T Marrugo y Aura/Todos los derechos reservados.
-</footer>
+    <div className="Container_Footer">
+      <img id="img1" src={Banner} alt="Esta es la banner" />
+
+      <div className="Pie_pagina">
+        <div className="Imagenes">
+        <img id="img1" src={Twitter} alt="Esta es la banner"/>
+        <img id="img1" src={Facebook} alt="Esta es la banner"/>
+        <img id="img1" src={Youtube} alt="Esta es la banner"/>
+        <img id="img1" src={Instagram} alt="Esta es la banner"/>
+        <img id="img1" src={Barras} alt="Esta es la banner"/>
+        </div>
+        <div className="Textos">
+        <p>
+           Copyright © 2021 Kuepa. Todos los derechos reservados. All rights reserved.
+        </p>
+        <p>
+           Desarrollado por Tatiana Marrugo y Aura Mejía.
+        </p>
+        </div>
+        
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
-
-
-
-import reportWebVitals from './reportWebVitals';
+import 'firebase/app'
 
 
 ReactDOM.render(
@@ -20,5 +17,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-reportWebVitals();
