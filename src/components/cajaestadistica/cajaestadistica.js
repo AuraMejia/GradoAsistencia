6079a4df76c1f526be5e40ff77dadb8ad29d375b
Khadija1103 <tatianamarrugo1103@gmail.com>
2021-08-05 13:18:13 -0500

    desarrollo de páginas ¿quién?

--- src/components/cajaestadistica/cajaestadistica.js
+++ src/components/cajaestadistica/cajaestadistica.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Botones from "../../Utils/Img/ilustraciones/botones.svg";
 import ReactDOM from 'react-dom';
 
 
@@ -7,9 +8,46 @@ import ReactDOM from 'react-dom';
 function Cajaestadistica() {
   return (
   
-  <div className="cajaestadistica">
- <h1>CAJA Estadistica</h1>
-  </div>
+    <div className="cajaindex">
+      <div className="Container_img">
+        <img id="img" src={Botones} alt="Esta es la img" />
+      </div>
+      <div className="Contenido">
+        
+        <p id="frase"><h1 id="Tittle">
+        ¿Quienes somos ?
+        </h1>
+        </p>
+        <p id="frase">
+        Bienvenidos a nuestro live de estadisticas
+        </p><br>
+        </br>
+        <p>Selecciona una de nuestras opciones</p>
+         
+  <div lassName="containerboton">     
+         <p id="frase">
+      <div className="Botones">
+                        <button>Aforo</button>
+               </div>
+      </p> 
+      <p id="frase">
+      <div className="Botones">
+                        <button>Genero</button>
+               </div>
+      </p> 
+      <p id="frase">
+      <div className="Botones">
+                        <button>Localidad</button>
+               </div>
+      </p> 
+       <p id="frase">
+      <div className="Botones">
+                        <button>Edad</button>
+               </div>
+      </p> 
+     </div> 
+</div>
+</div> 
   
 
   );
